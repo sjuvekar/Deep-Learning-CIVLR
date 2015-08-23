a9199de972422eb3e17fdbb860513dd7c3154d64
Sudeep Juvekar <sjuvekar@gmail.com>
2015-08-23 17:45:31 -0400

    output

--- assignment1/doall.lua
+++ assignment1/doall.lua
@@ -51,3 +51,4 @@ while true do
 	test()
 end
 
+dofile 'output'
---
+++ assignment1/output.lua
@@ -0,0 +1,24 @@
+require 'torch'
+require 'csvigo'
+
+local separator = ","
+
+if not testData then
+	test_raw = torch.load('data/test_32x32.t7', 'ascii')
+	testData = {
+		data = test_raw.X:transpose(3, 4),
+		y = test_raw.y[1],
+		size = function() return (#testData.data)[1] end
+	}	
+end
+
+local csvfile = csvigo.File("output/submission.csv", "w", separator)
+
+-- Write header
+csvfile:write({"Id", "Prediction"})
+
+for i = 1, testData:size() do
+	csvfile:write({i, testData.y[i]})
+end
+
+csvfile:close()
