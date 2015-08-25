9790de5e5c6d3e25c615371586ff14ba9d4ed1b4
Sudeep Juvekar <sjuvekar@gmail.com>
2015-08-25 08:14:01 -0400

    output

--- assignment1/output.lua
+++ assignment1/output.lua
@@ -1,5 +1,19 @@
 require 'torch'
 require 'csvigo'
+require 'nn'
+
+if not opt then
+   print '==> processing options'
+   cmd = torch.CmdLine()
+   cmd:text()
+   cmd:text('SVHN Saving to Output')
+   cmd:text()
+   cmd:text('Options:')
+   cmd:option('-save', 'results', 'subdirectory to save/log experiments in')
+   cmd:option('-type', 'double', 'type: double | float | cuda')
+   cmd:text()
+   opt = cmd:parse(arg or {})
+end
 
 local separator = ","
 
@@ -12,16 +26,23 @@ if not testData then
 	}	
 end
 
-if model then
-	testData.y = model:forward(testData.data)
-end
+local model_path = paths	.concat(opt.save, "model.net")
+model = torch.load(model_path)
 
 local csvfile = csvigo.File("output/submission.csv", "w", separator)
 
 -- Write header
 csvfile:write({"Id", "Prediction"})
 
+-- Predict and write data
 for i = 1, testData:size() do
+	if model then
+		local input = testData.data[i]
+		if opt.type == 'double' then input = input:double()
+		elseif opt.type == 'cuda' then input = input:cuda()
+		end
+		testData.y[i] = model:forward(input)
+	end
 	csvfile:write({i, testData.y[i]})
 end
 
